7c1f7e0a78fb1f4e9136f070c2272ad2e2ac16e8
takana-v <44311840+takana-v@users.noreply.github.com>
2022-02-22 04:57:45 +0900

    sklearnへの依存を外す

--- FixBuildPlugin.py
+++ FixBuildPlugin.py
@@ -40,6 +40,16 @@ class NuitkaPluginFixBuild(NuitkaPluginBase):
         elif module_name == "torch._jit_internal":
             source_code.replace(
                 'warnings.warn(f"Unable to retrieve source',
-                '# warnings.warn(f"Unable to retrieve source',
+                '#',
+            )
+        elif module_name == "librosa.decompose":
+            source_code.replace(
+                'import sklearn.decomposition',
+                '#'
+            )
+        elif module_name == "librosa.segment":
+            source_code.replace(
+                'import sklearn',
+                '#'
             )
         return source_code
